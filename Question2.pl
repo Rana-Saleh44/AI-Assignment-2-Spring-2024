466871dbb09b10fa4e727f35fa804c1b4b9f6247
Rana-Saleh44 <ranamhsaleh@gmail.com>
2024-05-02 11:11:14 +0300

    updates

--- Question2.pl
+++ Question2.pl
@@ -16,5 +16,12 @@ goal_position(Row, Column, Color):-
 set_board(Rows, Columns, Board):-
     assert(board(Rows, Columns, Board)).
 
-set_start_position(Row,Column, Col
+set_start_position(Row,Column, Color):-
+    retractall(start(_, _, _)),
+    assert(start(Row, Column, Color)).
+
+set_goal_position(Row, Column, Color):-
+    retractall(goal(_, _, _)),
+    assert(goal(Row, Column, Color)).
+
 
